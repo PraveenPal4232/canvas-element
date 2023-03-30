d14935f55d0a44111567fda62ff880650fb970a3
Praveen Pal <praveenpal4232@gmail.com>
2023-03-30 23:49:56 +0530

    Added basic layout

---
+++ src/layouts/Panel/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Panel() {
+  return (
+    <div className="panel">
+      <div class="panel_warp">
+        <h1>Panel</h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/layouts/Playground/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Playground() {
+  return (
+    <div className="playground">
+      <div class="playground_warp">
+        <h1>Playground</h1>
+      </div>
+    </div>
+  );
+}
